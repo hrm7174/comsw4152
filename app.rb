64416c08cb970a73d237ba6133491ef5c86590c1
hrm2134 Heidy Mendez <hrm2134@columbia.edu>
2025-09-09 21:28:28 +0000

    Initial Sinatra app with Gemfile, app.rb, config.ru

---
+++ app.rb
@@ -0,0 +1,10 @@
+require 'sinatra/base'
+
+class MyApp < Sinatra::Base
+  # allow any host in dev so Rack host protection doesn't block Codio
+  set :host_authorization, { permitted_hosts: [] }
+
+  get '/' do
+    '<!DOCTYPE html><html><body><h1>Hello World</h1></body></html>'
+  end
+end
